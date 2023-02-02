d159731e84d45f202502b7e1fca2b43607551121
semanticker <semanticker@gmail.com>
2023-02-02 23:15:22 +0900

    Initial commit

---
+++ src/main/java/org/example/WebApplicationServer.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.apache.catalina.startup.Tomcat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+public class WebApplicationServer {
+
+    private static final Logger log = LoggerFactory.getLogger(WebApplicationServer.class);
+
+    public static void main(String[] args) throws Exception {
+        String webDirLocation = "webapps/";
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8888);
+
+        tomcat.addWebapp("/", new File(webDirLocation).getAbsolutePath());
+        log.info("configuring app with basedir: {}", new File("./" + webDirLocation).getAbsolutePath());
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
