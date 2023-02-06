9935dd4154f2c3b37311a8895737b01f0571be9d
semanticker <semanticker@gmail.com>
2023-02-07 07:57:55 +0900

    패스워드 테스트 추가 결합도가 낮은 테스트 코드로 리팩토링

---
+++ src/test/java/org/example/UserTest.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @DisplayName("패스워드를 초기화 한다.")
+    @Test
+    void passwordTest() {
+        // given
+        User user = new User();
+
+        // when
+        //user.initPassword(new CorrectFixedPasswordGenerator());
+        user.initPassword(() -> "abcdefgh");
+
+        // then
+        assertThat(user.getPassword()).isNotNull();
+    }
+    @DisplayName("패스워드가 요구사항에 부합되지 않아 초기화 되지 않는다.")
+    @Test
+    void passwordTest2() {
+        // given
+        User user = new User();
+
+        // when
+        //user.initPassword(new WrongFixedPasswordGenerator());
+        user.initPassword(() -> "ab");
+
+        // then
+        assertThat(user.getPassword()).isNull();
+    }
+}
