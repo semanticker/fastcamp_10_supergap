de97c011bb1e6ea0c348cd6bc8fe224c7656d68e
semanticker <semanticker@gmail.com>
2023-02-06 21:58:43 +0900

    파라메터화 되어 있는 변수로 테스트

--- src/test/java/org/example/PasswordValidatorTest.java
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
@@ -14,7 +16,24 @@ public class PasswordValidatorTest {
     @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외가 발생하지 않는다.")
     @Test
     void validatePasswordTest() {
-            assertThatCode(() -> PasswordValidator.validate("serverwizard"))
-                    .doesNotThrowAnyException();
+        assertThatCode(() -> PasswordValidator.validate("serverwizard"))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("비밀번호가 8자 미만 또는 12자 초과하는 경우 IllegalArgumentException 예외가 발생한다.")
+    @Test
+    void validatePasswordTest2() {
+        assertThatCode(() -> PasswordValidator.validate("aabb"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(PasswordValidator.MESSAGE_EXCEPTION_WRONG_PASSWORD_LENGTH);
+    }
+
+    @DisplayName("비밀번호가 8자 미만 또는 12자 초과하는 경우 IllegalArgumentException 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"abcdefg","abcdefghijklm"})
+    void validatePasswordWithParameter(String password) {
+        assertThatCode(() -> PasswordValidator.validate(password))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(PasswordValidator.MESSAGE_EXCEPTION_WRONG_PASSWORD_LENGTH);
     }
 }
