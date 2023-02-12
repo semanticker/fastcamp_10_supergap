03310995840cdee6168771f2cb7cd377cdb3b31a
semanticker <semanticker@gmail.com>
2023-02-13 07:21:57 +0900

    컴파일 에러, 테스트 코드 에러 수정

--- src/main/java/org/example/calculate2/Calculator.java
+++ src/main/java/org/example/calculate2/Calculator.java
@@ -23,15 +23,6 @@ public class Calculator {
     }
 
 
-    public static int calculate2(int operand1, String operator, int operand2) {
-
-        return operators.stream()
-                .filter(operators -> operators.supports(operator))
-                .map(operators -> operators.calculate(operand1, operand2))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산이 아닙니다."));
-
-    }
 
 
 }
--- src/main/java/org/example/calculate2/MultiplicationOperator.java
+++ src/main/java/org/example/calculate2/MultiplicationOperator.java
@@ -1,6 +1,5 @@
 package org.example.calculate2;
 
-import org.example.calculate.NewArithmeticOperator;
 
 public class MultiplicationOperator  implements NewArithmeticOperator {
     @Override
@@ -9,8 +8,8 @@ public class MultiplicationOperator  implements NewArithmeticOperator {
     }
 
     @Override
-    public int calculate(int operand1, int operand2) {
-        return operand1 * operand2;
+    public int calculate(PositiveNumber operand1, PositiveNumber operand2) {
+        return operand1.toInt() * operand2.toInt();
     }
 }
 
