3ac3100f5157e695eb7036c3fe2b11be4174c3fb
semanticker <semanticker@gmail.com>
2023-02-20 07:40:44 +0900

    음식, 메뉴 추가 음식 테스트, 메뉴 테스트 코드 추가

---
+++ src/main/java/org/example/customer/MenuItem.java
@@ -0,0 +1,20 @@
+package org.example.customer;
+
+public class MenuItem {
+
+    private final String name;
+    private final int price;
+
+    public MenuItem(String name, int price) {
+        this.name = name;
+        this.price  = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
