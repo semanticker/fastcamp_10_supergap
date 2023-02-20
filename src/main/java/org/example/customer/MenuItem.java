46167517f67b26334d139afd6e5e6596518c4638
semanticker <semanticker@gmail.com>
2023-02-21 07:35:42 +0900

    메뉴, 메뉴아이템 테스트 코드 추가

--- src/main/java/org/example/customer/MenuItem.java
+++ src/main/java/org/example/customer/MenuItem.java
@@ -1,5 +1,7 @@
 package org.example.customer;
 
+import java.util.Objects;
+
 public class MenuItem {
 
     private final String name;
@@ -16,5 +18,22 @@ public class MenuItem {
 
     public int getPrice() {
         return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuItem menuItem = (MenuItem) o;
+        return getPrice() == menuItem.getPrice() && Objects.equals(getName(), menuItem.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getPrice());
+    }
+
+    public boolean matches(String name) {
+        return this.name.equals(name);
     }
 }
