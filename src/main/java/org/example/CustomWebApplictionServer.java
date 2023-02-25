8ac3ac20b0aaafb6407d24fb0b5d0dba2cd1db7e
semanticker <semanticker@gmail.com>
2023-02-25 18:02:24 +0900

    기본 서버 생성및 연결이 되었을때 로그에 남기도록 처리

---
+++ src/main/java/org/example/CustomWebApplictionServer.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class CustomWebApplictionServer {
+
+    private final int port;
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomWebApplictionServer.class);
+
+    public CustomWebApplictionServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            logger.info("[CustomerWebApplicationServer] started {} port.", port);
+
+            Socket clientSocket;
+            logger.info("[CustomerWebApplicationServer] wating for client.");
+
+            while ((clientSocket = serverSocket.accept()) != null) {
+
+                logger.info("[CustomerWebApplicationServer] client connected!");
+
+            }
+        }
+    }
+}
