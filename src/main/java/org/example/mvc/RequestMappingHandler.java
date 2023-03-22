cbc14240135b239ff22ee2da484c2e1b87bfd234
semanticker <semanticker@gmail.com>
2023-03-23 07:11:48 +0900

    프런트 컨트롤러 완성

---
+++ src/main/java/org/example/mvc/RequestMappingHandler.java
@@ -0,0 +1,23 @@
+package org.example.mvc;
+
+import org.example.mvc.controller.Controller;
+import org.example.mvc.controller.HomeController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestMappingHandler {
+    // URL path => key,
+    // /users -> UserController
+    private Map<String, Controller> mappings = new HashMap<>();
+
+    void init() {
+        mappings.put("/", new HomeController());
+    }
+
+    public Controller findHandler(String uriPath) {
+        return mappings.get(uriPath);
+    }
+
+
+}
